7039b586df4d055f3c133d9cf97f4097ef421ca9
pcalvove <pablocalvo@pablocalvo.name>
2016-04-07 15:30:42 +0200

    Workaround hasta resolver DNS kubernetes

--- src/controller/eureka_agent.py
+++ src/controller/eureka_agent.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf8 -*-
-import random
+import os
 from threading import Thread, Event
 import time
 
@@ -24,21 +24,26 @@ class EurekaAgent():
         if self.__output_handler_url_cache is None or \
                 self.__is_cache_refreshing_necessary():
 
-            self.__logger.info("Getting Output Handler URL")
-            self.__logger.info("Getting APP Instance dto List")
-            instance_dto_list = \
-                self.ec_client.get_all_instaces_of_app(OUTPUT_HANDLER_APP_NAME)
+            # self.__logger.info("Getting Output Handler URL")
+            # self.__logger.info("Getting APP Instance dto List")
+            # instance_dto_list = \
+            #     self.ec_client.get_all_instaces_of_app(OUTPUT_HANDLER_APP_NAME)
+            #
+            # self.__logger.info("Chosing Instance dto List")
+            # if instance_dto_list is None or len(instance_dto_list)<1:
+            #     self.__output_handler_url_cache = None
+            #
+            # instance_dto = random.choice(instance_dto_list)
+            # self.__logger.info("Building URL from APP Instance dto List")
+            # self.__output_handler_url_cache = \
+            #     self.__get_url_from_app_instance_dto(instance_dto)
+            #
+            # self.__last_cache_refresh_time = datetime.datetime.now()
 
-            self.__logger.info("Chosing Instance dto List")
-            if instance_dto_list is None or len(instance_dto_list)<1:
-                self.__output_handler_url_cache = None
-
-            instance_dto = random.choice(instance_dto_list)
-            self.__logger.info("Building URL from APP Instance dto List")
-            self.__output_handler_url_cache = \
-                self.__get_url_from_app_instance_dto(instance_dto)
-
-            self.__last_cache_refresh_time = datetime.datetime.now()
+            # TODO cuando se resuelva el tema del DNS de Kubernetes volver a la
+            # funcionalidad completa
+            self.__last_cache_refresh_time = "http://%s:80/transactions"%\
+                os.environ.get("MS_OUTPUT_SERVICE_SERVICE_HOST")
 
         return self.__output_handler_url_cache
 
